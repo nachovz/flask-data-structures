df1e14e58954674f600c2cecd5799da7e16a26bb
Ignacio Cordoba <icordoba@4geeks.co>
2019-03-08 23:24:58 +0000

    Initial state

--- app.py
+++ app.py
@@ -3,84 +3,20 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-person2 = {
-  "id": 2,
-  "name": "Jane",
-  "lastname": "Doe",
-  "age": 38,
-  "gender": "female",
-  "lucky_numbers": [ 2, 4, 6, 8]
-}
+person2 = {}
 
-person1 = {
-  "id": 1,
-  "name": "John",
-  "lastname": "Doe",
-  "age": 35,
-  "gender": "male",
-  "lucky_numbers": [ 7, 11, 13, 17]
-}
+person1 = {}
 
-family = {
-  "lastname": "Doe",
-  "members": [person1, person2]
-}
+family = {}
 
 
 @app.route('/', methods=['GET'])
 def hello():
-  lucky = []
-  for m in family["members"]:
-    lucky = lucky + m["lucky_numbers"]
-  
-  addAll = 0
-  for l in lucky:
-    addAll += l
-  
-  return jsonify({
-    "code": 200,
-    "data": {
-      "title": "The "+family["lastname"]+" Family",
-      "members": family["members"],
-      "lucky_numbers": lucky,
-      "sum_of_lucky": addAll
-    }
-  })
+  return 'family'
   
 @app.route('/member/<int:id>')
 def get_member(id):
-  if id > 0:
-    member = {}
-    for m in family["members"]:
-      if m["id"] == id:
-        member = m
-    
-    if 'name' in member:
-      return jsonify({"status_code": 200, "data": member})
-    else:
-      response = jsonify({"error": 400, "message":"no member found" })
-      response.status_code = 400
-      return response
-      
-      
-@app.route('/add', methods=['POST'])
-def add():
-  member = request.get_json() or {}
-  if 'name' not in member:
-    return jsonify({"status_code":400,"message":"missing name"})
-  
-  if 'lastname' not in member or member["lastname"] != "Doe":
-    return jsonify({"status_code":400,"message":"missing correct lastname"})
-    
-  if 'age' not in member or member["age"] < 1:
-    return jsonify({"status_code":400,"message":"missing age"})
-
-  if 'gender' not in member:
-    return jsonify({"status_code":400,"message":"missing gender"})
-  
-  if 'lucky_numbers' not in member:
-    return jsonify({"status_code":400,"message":"missing lucky_numbers"})
-    
+  return 'member'
   
 
 app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
